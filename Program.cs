f154449df512c2c9bdd33a0fcb82762fde658989
Aman Toumaj <tomajexpress@gmail.com>
2026-02-04 16:56:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient client = new OllamaApiClient(new Uri("http://localhost:11434"), "llama3.2");
+
+// send prompt to model
+string prompt = "What is Domain-Driven Design? Explain it in 20 words.";
+Console.WriteLine($"user prompt -> {prompt}");
+
+ChatResponse response = await client.GetResponseAsync(prompt);
+Console.WriteLine($"model response -> {response}");
+Console.WriteLine($"Tokens used: in= {response.Usage?.InputTokenCount}, out={response.Usage?.OutputTokenCount}");
